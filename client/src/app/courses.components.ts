069cc622e74b2e347a7bb89978d41e0ac51d3d2c
dienul <haqdienul@gmail.com>
2020-03-22 13:58:24 +0700

    update

--- client/src/app/courses.components.ts
+++ client/src/app/courses.components.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core' ;
 
 @Component({
     selector : 'courses',
-    template : '<h3>Saya Dienul Haq</h3>'
+    template : `
+                <h2> {{ getTitle() }} </h2>
+                <ul>
+                    <li *ngFor="let course of courses"> 
+                        {{course}}
+                    </li>
+                <ul>
+            `
 })
 
 export class CoursesComponent {
+    title = "List of courses" ;
+    courses = ["course 1", "course 2", "course 3"]
 
+    getTitle(){
+        return this.title
+    }
 }
