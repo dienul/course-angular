b095dfce1d3c6ea4fc4576b0d46f54fdb9a2295c
dienul <haqdienul@gmail.com>
2020-03-22 13:28:56 +0700

    update

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { CoursesComponent } from './courses.components'
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { CourseComponent } from './course/course.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/src/app/courses.components.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core' ;
+
+@Component({
+    selector : 'courses',
+    template : '<h3>Saya Dienul Haq</h3>'
+})
+
+export class CoursesComponent {
+
+}
